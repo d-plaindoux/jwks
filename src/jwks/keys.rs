45bd7f1f90878226e9f6b4a5195fd49453b08998
Didier Plaindoux <d.plaindoux@free.fr>
2018-09-15 09:07:35 +0200

    prototype for key generation

--- src/jwks/keys.rs
+++ src/jwks/keys.rs
@@ -5,29 +5,26 @@ use openssl::error::ErrorStack;
 
 use jwks::data::{JSONWebKey, RSAPublicSpecification, ECPublicSpecification};
 
+// -------------------------------------------------------------------------------------------------
+
 pub trait KeyProvider<E> {
-    fn public_key(&self) -> Result<E, ErrorStack>;
-    fn private_key(&self) -> Option<()>;
+    fn key(&self) -> Result<E, ErrorStack>;
 }
 
 // -------------------------------------------------------------------------------------------------
 
 impl KeyProvider<Rsa<Public>> for JSONWebKey<RSAPublicSpecification> {
-    fn public_key(&self) -> Result<Rsa<Public>, ErrorStack> {
-        unimplemented!()
-    }
-
-    fn private_key(&self) -> Option<()> {
+    fn key(&self) -> Result<Rsa<Public>, ErrorStack> {
         unimplemented!()
     }
 }
+
+// -------------------------------------------------------------------------------------------------
 
 impl KeyProvider<EcKey<Public>> for JSONWebKey<ECPublicSpecification> {
-    fn public_key(&self) -> Result<EcKey<Public>, ErrorStack> {
-        unimplemented!()
-    }
-
-    fn private_key(&self) -> Option<()> {
+    fn key(&self) -> Result<EcKey<Public>, ErrorStack> {
         unimplemented!()
     }
 }
+
+// -------------------------------------------------------------------------------------------------
